a5d30c659c9b40eb3cc10b672a7c612822017cc3
Zia Saafir <ziagit11@gmail.com>
2026-01-22 21:05:27 +0500

    Update vite.config.jssome change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Must match repository name
+  base: '/', // Must match repository name
   server: {
     port: 3000,
     open: true
